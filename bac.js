9b9c6bdfb095832e66de4bfa78253c68e3672d88
skillup <test@test.com>
2019-11-05 19:20:54 +0200

    v1

---
+++ bac.js
@@ -0,0 +1,42 @@
+// 1 number
+var a = 15;
+
+// 2 string
+var helloWorld = 'hi';
+
+// 3 boolean
+var isSum = 5 > 7;
+
+// 4 undefined
+var myUndefined;
+
+// 5 null
+var myNull = document.querySelector('body2');
+
+// 6 Object
+
+// array
+var cities = [
+    'Dnipro',
+    'Kiev',
+    'Odessa'
+];
+
+var user = {
+    age: 25,
+    email: 'vasa@gmail.com',
+    name: 'Vasa',
+};
+
+user['index'] = 49000;
+
+var users = [
+    { name: 'Vasa', age: 25, phones: ['+388', '+788'] },
+    { name: 'Pit', age: 35, phone: '' },
+    { name: 'Ann', age: 36 },
+];
+
+
+console.log(users[0].phones[1]);
+
+
---
+++ main.js
@@ -0,0 +1,48 @@
+'use strict'
+
+function format(response) {
+    return response.json();
+}
+
+function render1(posts) {
+    var html = '';
+    for (var post of posts) {
+        html += '<div class="post">';
+        html += '<b>' + post.data_update + '</b>';
+        html += '<h2>' + post.title + '</h2>';
+        html += '<p>' + post.description + '</p>';
+        html += '</div>'
+    }
+    return html;
+}
+
+function render2(str) {
+    var container = document.querySelector('#list-posts');
+    container.innerHTML = str;
+    console.dir(container);
+}
+
+function showPosts() {
+    fetch('http://blog.api.axenov-it.com/posts')
+        .then(format)
+        .then(render1)
+        .then(render2);
+}
+
+var btn = document.querySelector('#render-posts');
+
+
+var isOpen = false;
+
+function toggle() {
+    var container = document.querySelector('#list-posts');
+    if (isOpen) {
+        container.innerHTML = "";
+        isOpen = false;
+    } else {
+        showPosts();
+        isOpen = true;
+    }
+}
+
+btn.onclick = toggle;
